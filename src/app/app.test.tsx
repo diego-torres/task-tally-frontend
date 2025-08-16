bfa89322f26b61ee924df94dc9a830d4adf71779
Diego Alejandro Torres Fuerte <dtorresf@redhat.com>
2025-08-16 15:33:04 -0600

    refactor: remove skipped test for sidebar visibility on smaller viewports

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -17,19 +17,6 @@ describe('App tests', () => {
     expect(screen.getByRole('button', { name: 'Global navigation' })).toBeVisible();
   });
 
-  // I'm fairly sure that this test not going to work properly no matter what we do since JSDOM doesn't actually
-  // draw anything. We could potentially make something work, likely using a different test environment, but
-  // using Cypress for this kind of test would be more efficient.
-  it.skip('should hide the sidebar on smaller viewports', () => {
-    Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 600 });
-
-    render(<App />);
-
-    window.dispatchEvent(new Event('resize'));
-
-    expect(screen.queryByRole('link', { name: 'Home' })).not.toBeInTheDocument();
-  });
-
   it('should expand the sidebar on larger viewports', () => {
     render(<App />);
 
